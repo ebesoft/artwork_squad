d627efa6c9087bb350b0b09ed4fa0cc4167809d8
Ermackyes <directordesoftware@gmail.com>
2021-12-06 10:01:51 -0500

    Firestore

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:artwork_squad/app/app.dart';
 import 'package:artwork_squad/app/controllers/chatDetail_controller.dart';
 import 'package:artwork_squad/app/controllers/controllerRealtime.dart';
+import 'package:artwork_squad/app/controllers/firestore_controller.dart';
 import 'package:artwork_squad/app/controllers/login_controller.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
@@ -12,5 +13,6 @@ void main() async {
   Get.put(LoginController());
   Get.put(RealtimeController());
   Get.put(ChatDetailController());
+  Get.put(FirestoreController());
   runApp(App());
 }
