4e320d50801b2acd80dbd8dfc0724025d4e2b0ec
LoreNaRyV <mlrey@uninorte.edu.co>
2021-12-21 15:54:49 -0500

    add location

--- lib/app/bindings/location_binding.dart
+++ lib/app/bindings/location_binding.dart
@@ -1,4 +1,4 @@
-import 'package:artwork_squad/app/controllers/location_controller.dart';
+import 'package:artwork_squad/app/controllers/locations.dart';
 import 'package:get/get.dart';
 
 class LocationBinding implements Bindings {
